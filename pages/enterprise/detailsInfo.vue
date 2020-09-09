88408108aa1b4540d631a120281cb21ef954e057
kaiwangky <474529914@qq.com>
2020-09-09 09:23:59 +0800

    首页 企业排行

---
+++ pages/enterprise/detailsInfo.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- 企业门户详情 -->
+	<view>
+		企业门户详情
+	</view>
+</template>
+
+<script>
+	export default {
+        onReady(){
+            uni.setNavigationBarTitle({
+                title: '湖南省云上茶叶有限公司'
+            })
+        },
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
